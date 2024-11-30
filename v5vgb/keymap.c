b807287808cc619ce3256bc6d487ce39e566a773
Tim Wolff <timtwotoes@me.com>
2024-11-30 02:25:43 +0100

    Remove NULL from key_overrides array

--- v5vgb/keymap.c
+++ v5vgb/keymap.c
@@ -157,8 +157,7 @@ void matrix_scan_user(void) {
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 const key_override_t **key_overrides = (const key_override_t *[]) {
-  &delete_key_override,
-  NULL
+  &delete_key_override
 };
 // Key Overrides End
 
