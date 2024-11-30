8f7441b0428d66c6f9467582d2d59460bf5fadc0
Tim Wolff <timtwotoes@me.com>
2024-11-30 01:08:19 +0100

    Modify key overrides for older version of QMK

--- v5vgb/keymap.c
+++ v5vgb/keymap.c
@@ -13,8 +13,9 @@
 // Key Overrides Begin
 const key_override_t delte_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
-const key_override_t *key_overrides[] = {
-  &delete_key_override
+const key_override_t **key_overrides[] = (const key_override_t *[]) {
+  &delete_key_override,
+  NULL
 };
 // Key Overrides End
 
