806d4552ec4a1ff2d71741cd33aae988e831f481
Tim Wolff <timtwotoes@me.com>
2024-12-06 17:30:04 +0100

    Fix L in keymap

--- v5vgb/keymap.c
+++ v5vgb/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     DK_DIAE,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           DK_ACUT,        
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           DK_ARNG,        
-    DK_PLUS,        CKC_A,          CKC_S,          CKC_D,          CKC_F,          KC_G,                                           KC_H,           CKC_J,          CKC_K,          CKC_K,          CKC_AE,         DK_OSTR,        
+    DK_PLUS,        CKC_A,          CKC_S,          CKC_D,          CKC_F,          KC_G,                                           KC_H,           CKC_J,          CKC_K,          CKC_L,          CKC_AE,         DK_OSTR,        
     DK_LABK,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         DK_MINS,        DK_QUOT,        
                                                     KC_ENTER,       LT(1,KC_BSPC),                                  LT(2,KC_ESCAPE),KC_SPACE
   ),
