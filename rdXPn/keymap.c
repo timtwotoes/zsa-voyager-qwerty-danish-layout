a312f3841229ca7175f15b453416497704c2fe02
Tim Wolff <timtwotoes@me.com>
2025-07-06 12:22:32 +0200

    Add key override for delete

--- rdXPn/keymap.c
+++ rdXPn/keymap.c
@@ -119,5 +119,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+// Key Overrides
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, LT(1,KC_BSPC), KC_DELETE);
 
+const key_override_t **key_overrides = (const key_override_t *[]) {
+  &delete_key_override,
+  NULL
+};
 
