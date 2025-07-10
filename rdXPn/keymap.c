a29b695f4f95da8112f1c422033daaa7a68a1784
Tim Wolff <timtwotoes@me.com>
2025-07-11 01:02:57 +0200

    Disable CAPS_WORD_INVERT_ON_SHIFT and handle shift manually

--- rdXPn/keymap.c
+++ rdXPn/keymap.c
@@ -208,6 +208,8 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
 
 
 bool caps_word_press_user(uint16_t keycode) {
+  const bool shift_is_held = (get_mods() & MOD_MASK_SHIFT);
+
   switch (keycode) {  
     // Keycodes that continue Caps Word
     case KC_A ... KC_Z:
@@ -218,6 +220,10 @@ bool caps_word_press_user(uint16_t keycode) {
       return true;
 
     case KC_1 ... KC_0:
+      return shift_is_held == false;
+
+    case KC_LSFT:
+    case KC_RSFT:
     case KC_BSPC:
     case KC_DEL:
     case DK_MINS: // This is dash
