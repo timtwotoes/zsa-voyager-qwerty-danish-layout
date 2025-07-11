759dd34a7f12e4f478908de145f7d6e88698d7ef
Tim Wolff <timtwotoes@me.com>
2025-07-11 23:23:31 +0200

    Never set weak mods

--- rdXPn/keymap.c
+++ rdXPn/keymap.c
@@ -242,10 +242,10 @@ bool caps_word_press_user(uint16_t keycode) {
     case DK_OSTR: // ø
     case DK_ARNG: // å
     case DK_MINS: // This is minus and dash
-      if ((get_mods() & MOD_MASK_SHIFT) == false) {
-        add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to the next key.
-      }
-      return true;
+//      if ((get_mods() & MOD_MASK_SHIFT) == false) {
+//        add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to the next key.
+//      }
+//      return true;
 
     case KC_1 ... KC_0:
       return (get_mods() & MOD_MASK_SHIFT) == false;
